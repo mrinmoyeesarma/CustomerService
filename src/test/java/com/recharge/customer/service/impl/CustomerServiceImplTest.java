5ccd50b5f56cdbc75c5508a4de431e4225d131f4
SUD <sudhansu.ace@gmail.com>
2023-01-11 00:21:26 +0530

    Updated Tests

--- src/test/java/com/recharge/customer/service/impl/CustomerServiceImplTest.java
+++ src/test/java/com/recharge/customer/service/impl/CustomerServiceImplTest.java
@@ -1,21 +1,26 @@
 package com.recharge.customer.service.impl;
 
 import com.recharge.customer.entity.RechargePlan;
+import com.recharge.customer.entity.Subscription;
+import com.recharge.customer.feignClient.AdminClient;
 import com.recharge.customer.repository.RechargePlanRepository;
+import com.recharge.customer.repository.SubscriptionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerServiceImplTest {
+    private AdminClient adminClient;
     private CustomerServiceImpl customerService;
     private RechargePlanRepository rechargePlanRepository;
+    private SubscriptionRepository subscriptionRepository;
 
     @BeforeEach
     void setup() {
@@ -23,10 +28,13 @@ public class CustomerServiceImplTest {
         customerService = new CustomerServiceImpl();
 
         // Mockito
+        adminClient = Mockito.mock(AdminClient.class);
         rechargePlanRepository = Mockito.mock(RechargePlanRepository.class);
+        subscriptionRepository = Mockito.mock(SubscriptionRepository.class);
 
         // Reflection Test Utils
         ReflectionTestUtils.setField(customerService, "rechargePlanRepository", rechargePlanRepository);
+        ReflectionTestUtils.setField(customerService, "subscriptionRepository", subscriptionRepository);
     }
 
     @Test
@@ -80,5 +88,46 @@ public class CustomerServiceImplTest {
 
         // Assertions
         assertEquals(1, plans.size());
+    }
+
+    @Test
+    void testUpdateSubscriptionStatus() {
+        Subscription subscription = new Subscription();
+        subscription.setRecharge_id(1);
+
+        Mockito.when(subscriptionRepository.findById(Mockito.any())).thenReturn(Optional.of(subscription));
+        Mockito.when(subscriptionRepository.save(Mockito.any())).thenReturn(subscription);
+
+        Subscription sub = customerService.updateSubscriptionStatus(1, "Cancel");
+        assertEquals(sub, subscription);
+    }
+
+    @Test
+    void testGetAvailablePlansById() {
+        Subscription subscription = new Subscription();
+        subscription.setRecharge_id(1);
+
+        Mockito.when(subscriptionRepository.findById(Mockito.any())).thenReturn(Optional.of(subscription));
+
+        Subscription sub = customerService.getAvailablePlansById(1);
+        assertEquals(sub, subscription);
+    }
+
+    @Test
+    void testCreateRecharge() {
+        RechargePlan rechargePlan = new RechargePlan();
+        rechargePlan.setPlan_id(1);
+        rechargePlan.setNetworkProvider("Hello");
+        rechargePlan.setAmount(120);
+        rechargePlan.setValidity(90);
+        rechargePlan.setAvailable(true);
+        Mockito.when(rechargePlanRepository.findById(Mockito.any())).thenReturn(Optional.of(rechargePlan));
+
+        Subscription subscription = new Subscription();
+        subscription.setRecharge_id(1);
+        Mockito.when(subscriptionRepository.save(Mockito.any())).thenReturn(subscription);
+
+        Subscription sub = customerService.createRecharge(1, "Hello");
+        assertEquals(sub, subscription);
     }
 }
